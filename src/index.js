06b464449913995407b6cb39daa6d62151845225
FireYourGuns <chazovby@gmail.com>
2020-10-02 16:16:46 +0300

    feat: implement min max avg but without 0

--- src/index.js
+++ src/index.js
@@ -1,14 +1,21 @@
 
 exports.min = function min (array) {
-  array.sort((a, b) => (a - b));
-  return array[0];
+  if (array == [] || !array) {
+    return 0
+  } ;
+    array.sort((a, b) => (a - b));
+    return array[0];
 }
 
 exports.max = function max (array) {
-  array.sort((a, b) => (b - a));
-  return array[0];
+  if (array == [] || !array) {
+    return 0
+  }; 
+    array.sort((a, b) => (b - a));
+    return array[0];
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  return array.reduce((a, b) => a + b, 0) / array.length;
+  
 }
