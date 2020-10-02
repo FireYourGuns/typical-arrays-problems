f86c870fbc8e6d2cc5615e67ef6a22e0a8c22ccc
FireYourGuns <chazovby@gmail.com>
2020-10-02 19:47:36 +0300

    feat: task finish

--- src/index.js
+++ src/index.js
@@ -1,21 +1,24 @@
 
 exports.min = function min (array) {
-  if (array == [] || !array) {
-    return 0
-  } ;
+  if (typeof array == 'undefined' || array.length == 0) {
+    return 0;
+  }
     array.sort((a, b) => (a - b));
     return array[0];
 }
 
 exports.max = function max (array) {
-  if (array == [] || !array) {
-    return 0
-  }; 
+  if (typeof array == 'undefined' || array.length == 0) {
+    return 0;
+  }
     array.sort((a, b) => (b - a));
     return array[0];
 }
 
 exports.avg = function avg (array) {
+  if (typeof array == 'undefined' || array.length == 0) {
+    return 0;
+  }
   return array.reduce((a, b) => a + b, 0) / array.length;
   
 }
